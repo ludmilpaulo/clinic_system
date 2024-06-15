1897753246b5529159b1684e78240c25700cf693
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2024-06-15 11:01:09 +0200

    error

--- orders/pdf.py
+++ orders/pdf.py
@@ -9,7 +9,7 @@ from .models import Order
 def generate_order_pdf(order, request):
     about_us = AboutUs.objects.first()  # Assuming there's only one AboutUs entry
     html_string = render_to_string('invoice_template.html', {'order': order, 'about_us': about_us})
-    
+
     html = HTML(string=html_string, base_url=request.build_absolute_uri())
     pdf_file = html.write_pdf()
 
