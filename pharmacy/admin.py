50bc788880eedb20590aaeae7f1122ca442cca1a
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2024-06-15 03:49:15 +0200

    sleep

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.shortcuts import render, redirect
-from django.urls import path
 
 from pharmacy.forms import MultipleImageUploadForm
 from .models import Drug, Image, Prescription, PrescriptionDrug, ConsultationCategory
@@ -29,33 +27,19 @@ class PrescriptionDrugAdmin(admin.ModelAdmin):
     search_fields = ('prescription__prescription_number', 'drug__name')
 
 class ImageAdmin(admin.ModelAdmin):
-    change_list_template = "admin/multiple_upload.html"
+    form = MultipleImageUploadForm
 
-    def get_urls(self):
-        urls = super().get_urls()
-        custom_urls = [
-            path('multiple_upload/', self.admin_site.admin_view(self.multiple_upload), name='multiple_image_upload'),
-        ]
-        return custom_urls + urls
+    def save_model(self, request, obj, form, change):
+        files = request.FILES.getlist('image')
+        for f in files:
+            instance = Image(image=f)
+            instance.save()
 
-    def multiple_upload(self, request):
-        if request.method == 'POST':
-            form = MultipleImageUploadForm(request.POST, request.FILES)
-            files = request.FILES.getlist('images')
-            if form.is_valid():
-                for f in files:
-                    instance = Image(image=f)
-                    instance.save()
-                self.message_user(request, "Images uploaded successfully")
-                return redirect("admin:pharmacy_image_changelist")
-        else:
-            form = MultipleImageUploadForm()
-        context = {
-            'form': form,
-            'opts': self.model._meta,
-            'app_label': self.model._meta.app_label,
-        }
-        return render(request, "admin/multiple_upload.html", context)
+    def save_related(self, request, form, formsets, change):
+        pass
+
+    def response_add(self, request, obj, post_url_continue=None):
+        return super().response_add(request, obj, post_url_continue)
 
 custom_admin_site.register(ConsultationCategory)
 custom_admin_site.register(Drug, DrugAdmin)
@@ -64,5 +48,4 @@ custom_admin_site.register(Image, ImageAdmin)
 
 admin.site.register(ConsultationCategory)
 admin.site.register(Drug, DrugAdmin)
-admin.site.register(Image, ImageAdmin) 
 admin.site.register(PrescriptionDrug, PrescriptionDrugAdmin)
